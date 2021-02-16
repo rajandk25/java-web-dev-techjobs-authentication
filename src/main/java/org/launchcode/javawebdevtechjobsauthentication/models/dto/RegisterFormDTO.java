65e507e82fd4bfbf55a7a6c76a17fb89cd2f5a7c
rajandk25 <rajandk25@users.noreply.github.com>
2021-02-16 11:34:04 -0600

    half done

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/dto/RegisterFormDTO.java
@@ -0,0 +1,14 @@
+package org.launchcode.javawebdevtechjobsauthentication.models.dto;
+
+public class RegisterFormDTO {
+
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+}
